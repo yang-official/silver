98e9c8a290c402e6faf0b194ba7bfec969df6728
User <yang.official@gmail.com>
2020-04-01 15:30:32 -0700

    first commit

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+ title: "Silver 1 Miner",
+ description: 'Going the other way down',
+ base: '/',
+ themeConfig: {
+     nav: [
+        { text: 'Home', link: '/' },
+     ],
+ }
+}
---
+++ docs/index.md
@@ -0,0 +1,9 @@
+---
+title: Home
+home: true
+---
+
+# Quick Links
+- [Books](https://gumroad.com/trueorigin)
+- [Translations](https://threestatesrecords.com)
+- [Game](https://takensky.wordpress.com)
